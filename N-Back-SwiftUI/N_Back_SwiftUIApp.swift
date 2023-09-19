dda34d0247d2502eadd1311c82191b57297ff9dc
Jonas Willén <jwi@kth.se>
2023-09-19 10:17:25 +0200

    Initial Commit

---
+++ N-Back-SwiftUI/N_Back_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  N_Back_SwiftUIApp.swift
+//  N-Back-SwiftUI
+//
+//  Created by Jonas Willén on 2023-09-19.
+//
+
+import SwiftUI
+
+@main
+struct N_Back_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
